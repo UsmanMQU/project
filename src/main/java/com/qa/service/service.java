32545dd9410b44611c0ad1f13d33f717d9804768
Framsida <Joshuastait@gmail.com>
2018-08-24 10:07:32 +0100

    First commit

--- src/main/java/com/qa/service/service.java
+++ src/main/java/com/qa/service/service.java
@@ -3,9 +3,12 @@ import Bae1.application.account;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONObject;
+
 public class service 
 {
 	private int counter = 1;
+	private JSONObject json;
 	private Map<Integer, account> map;
 	public service()
 	{
@@ -14,12 +17,29 @@ public class service
 	
 	public void addAccount(account acc)
 	{
-		map.put(counter, acc);
-		counter++;
+		map.put(counter, acc);    	
+    	counter++;
 	}
 	
 	public account retrive(int no)
 	{
 		return map.get(no);
+	}
+	
+	public String a()
+	{
+		json = new JSONObject(map);
+		return json.toString();		
+	}
+	public int times()
+	{
+		String[] accountArray = json.toString().split("firstName=John");
+	    int counter=-1;
+	    for(String name : accountArray)
+	    {
+	   	   counter++;    	   
+	    }
+	    System.out.println("There are " + counter + " John(s)");
+		return counter;
 	}
 }
---
+++ src/test/java/Bae1/test/TestC.java
@@ -0,0 +1,8 @@
+package Bae1.test;
+
+import junit.framework.TestCase;
+
+public class TestC extends TestCase 
+{
+
+}
